6c121de74beda94ed5bdf1be9b79c9995b44735c
UltramarineW <2021113679@stu.hit.edu.cn>
2023-10-05 21:21:15 +0800

    feature(10.5): add gflags

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include "proxy_server.h"
+#include "gflags/gflags.h"
+
+DEFINE_int32(port, 10086, "Proxy Server Port");
 
 int main(int argc, char *argv[]) {
-    ProxyServer server = ProxyServer(10);
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+    ProxyServer server = ProxyServer(FLAGS_port);
     server.init();
     return 0;
 }
